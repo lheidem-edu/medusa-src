61f612dc130b5dac480282a3370096c5ebbc614c
Luca Heidemann <kontakt@lheidem.net>
2025-05-06 12:14:22 +0200

    Add `Device` class

---
+++ src/Medusa.Domain/Devices/Device.cs
@@ -0,0 +1,55 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Medusa.Domain.Devices;
+
+[Table("devices", Schema = "public")]
+public class Device : IAuditable, IIdentifiable
+{
+    /// <summary>
+    ///     The unique identifier for the device.
+    /// </summary>
+    [Column("device_id")]
+    public required Guid Id { get; init; }
+
+    /// <summary>
+    ///     The unique identifier for the tenant associated with this device.
+    /// </summary>
+    [Column("device_tenant_id")]
+    public required Guid TenantId { get; set; }
+
+    /// <summary>
+    ///     The serial number of the device.
+    /// </summary>
+    [Column("device_serial_number")]
+    public required string SerialNumber { get; set; }
+
+    /// <summary>
+    ///     The name of the device.
+    /// </summary>
+    [Column("device_name")]
+    public required string Name { get; set; }
+
+    /// <summary>
+    ///     The description of the device.
+    /// </summary>
+    [Column("device_description")]
+    public string? Description { get; set; }
+
+    /// <summary>
+    ///     The location of the device.
+    /// </summary>
+    [Column("device_location")]
+    public string? Location { get; set; }
+
+    /// <summary>
+    ///     The date and time when the device was created.
+    /// </summary>
+    [Column("device_created_at")]
+    public required DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+
+    /// <summary>
+    ///     The date and time when the device was last updated.
+    /// </summary>
+    [Column("device_updated_at")]
+    public required DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
