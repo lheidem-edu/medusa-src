353d2cfe8998b08b080c12ce35e087f871e2af0f
Luca Heidemann <kontakt@lheidem.net>
2025-05-06 22:47:57 +0200

    Remove unused `TenantId` property

--- src/Medusa.Presentation/Workplaces/CreateWorkplaceModel.cs
+++ src/Medusa.Presentation/Workplaces/CreateWorkplaceModel.cs
@@ -4,9 +4,6 @@ namespace Medusa.Presentation.Workplaces;
 
 public class CreateWorkplaceModel
 {
-    [JsonPropertyName("workplace_tenant_id")]
-    public required Guid TenantId { get; set; }
-
     [JsonPropertyName("workplace_enabled")]
     public required bool Enabled { get; set; }
 
