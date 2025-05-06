14cb7a845fdd19693f603ddfca1c27bae5248556
Luca Heidemann <kontakt@lheidem.net>
2025-05-06 12:19:56 +0200

    Add `DeviceModel` and `TenantModel` classes with JSON serialization attributes

---
+++ Medusa.Presentation/Devices/DeviceModel.cs
@@ -0,0 +1,30 @@
+using System.Text.Json.Serialization;
+
+namespace Medusa.Presentation.Devices;
+
+public class DeviceModel
+{
+    [JsonPropertyName("device_id")]
+    public required Guid Id { get; init; }
+
+    [JsonPropertyName("device_tenant_id")]
+    public required Guid TenantId { get; set; }
+
+    [JsonPropertyName("device_serial_number")]
+    public required string SerialNumber { get; set; }
+
+    [JsonPropertyName("device_name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("device_description")]
+    public string? Description { get; set; }
+
+    [JsonPropertyName("device_location")]
+    public string? Location { get; set; }
+
+    [JsonPropertyName("device_created_at")]
+    public required DateTime CreatedAt { get; init; }
+
+    [JsonPropertyName("device_updated_at")]
+    public required DateTime UpdatedAt { get; set; }
+}
---
+++ Medusa.Presentation/Tenants/TenantModel.cs
@@ -0,0 +1,21 @@
+using System.Text.Json.Serialization;
+
+namespace Medusa.Presentation.Tenants;
+
+public class TenantModel
+{
+    [JsonPropertyName("tenant_id")]
+    public required Guid Id { get; init; }
+
+    [JsonPropertyName("tenant_country")]
+    public required string Country { get; set; }
+
+    [JsonPropertyName("tenant_name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("tenant_created_at")]
+    public required DateTime CreatedAt { get; init; }
+
+    [JsonPropertyName("tenant_updated_at")]
+    public required DateTime UpdatedAt { get; set; }
+}
