1e903cf79199ec94dbf4379f257300fc67db8f32
Luca Heidemann <kontakt@lheidem.net>
2025-05-06 22:44:37 +0200

    Remove unused `DeviceId` property

--- src/Medusa.Presentation/Devices/CreateDeviceActivityModel.cs
+++ src/Medusa.Presentation/Devices/CreateDeviceActivityModel.cs
@@ -4,9 +4,6 @@ namespace Medusa.Presentation.Devices;
 
 public class CreateDeviceActivityModel
 {
-    [JsonPropertyName("device_id")]
-    public required Guid DeviceId { get; set; }
-
     [JsonPropertyName("device_activity_type")]
     public required string Type { get; set; }
 }
