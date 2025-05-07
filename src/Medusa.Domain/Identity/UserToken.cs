6418b7925952da6ca87a10fa0600e171eedb0796
Luca Heidemann <kontakt@lheidem.net>
2025-05-07 21:56:11 +0200

    Add column attributes to `UserToken` properties

--- src/Medusa.Domain/Identity/UserToken.cs
+++ src/Medusa.Domain/Identity/UserToken.cs
@@ -11,30 +11,36 @@ public class UserToken : IAuditable, IIdentifiable
     /// <summary>
     ///     The unique identifier of the token.
     /// </summary>
+    [Column("user_token_id")]
     public Guid Id { get; init; } = Guid.NewGuid();
 
     /// <summary>
     ///     The unique identifier of the user associated with this token.
     /// </summary>
+    [Column("user_id")]
     public required Guid UserId { get; set; }
 
     /// <summary>
     ///     The token hash.
     /// </summary>
+    [Column("user_token_hash")]
     public required string Hash { get; set; }
 
     /// <summary>
     ///     The date and time when the token expires.
     /// </summary>
+    [Column("user_token_expires_at")]
     public required DateTime ExpiresAt { get; set; }
 
     /// <summary>
     ///     The date and time when the token was created.
     /// </summary>
+    [Column("user_token_created_at")]
     public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
 
     /// <summary>
     ///     The date and time when the token was last updated.
     /// </summary>
+    [Column("user_token_updated_at")]
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
