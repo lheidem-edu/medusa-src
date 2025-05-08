c44130e098b899bb97975d8c9b5313b6f245262c
Luca Heidemann <kontakt@lheidem.net>
2025-05-08 09:57:29 +0200

    Add `UserTokenModel` class

---
+++ src/Medusa.Presentation/Identity/UserTokenModel.cs
@@ -0,0 +1,21 @@
+using System.Text.Json.Serialization;
+
+namespace Medusa.Presentation.Identity;
+
+public class UserTokenModel
+{
+    [JsonPropertyName("user_token_id")]
+    public required Guid Id { get; init; }
+
+    [JsonPropertyName("user_id")]
+    public required Guid UserId { get; set; }
+
+    [JsonPropertyName("user_token_expires_at")]
+    public required DateTime ExpiresAt { get; set; }
+
+    [JsonPropertyName("user_token_created_at")]
+    public DateTime CreatedAt { get; init; }
+
+    [JsonPropertyName("user_token_updated_at")]
+    public DateTime UpdatedAt { get; init; }
+}
