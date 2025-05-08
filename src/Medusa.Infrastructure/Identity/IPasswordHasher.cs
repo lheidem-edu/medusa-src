7bff549558d6bf5fcae199e2018b760a48c80485
Luca Heidemann <kontakt@lheidem.net>
2025-05-08 09:17:00 +0200

    Refactor `PasswordHasher` to use argon2 algorithm

--- src/Medusa.Infrastructure/Identity/IPasswordHasher.cs
+++ src/Medusa.Infrastructure/Identity/IPasswordHasher.cs
@@ -3,17 +3,19 @@ namespace Medusa.Infrastructure.Identity;
 public interface IPasswordHasher
 {
     /// <summary>
-    ///     Hashes a password using bcrypt.
+    ///     Hashes a password using argon2.
     /// </summary>
     /// <param name="password">The password to hash.</param>
+    /// <param name="cancellationToken">The cancellation token to observe while waiting for the task to complete.</param>
     /// <returns>The hashed password.</returns>
-    string ComputeHash(string password);
+    Task<string> ComputeHash(string password, CancellationToken cancellationToken = default);
 
     /// <summary>
-    ///     Verifies a password against a hashed password using bcrypt.
+    ///     Verifies a password against a hashed password using argon2.
     /// </summary>
     /// <param name="password">The password to verify.</param>
-    /// <param name="hash">The hash to verify against.</param>
+    /// <param name="saltedHash">The hash to verify against.</param>
+    /// <param name="cancellationToken">The cancellation token to observe while waiting for the task to complete.</param>
     /// <returns>A boolean indicating whether the password matches the hashed password.</returns>
-    bool VerifyHash(string password, string hash);
+    Task<bool> VerifyHash(string password, string saltedHash, CancellationToken cancellationToken = default);
 }
--- src/Medusa.Infrastructure/Identity/PasswordHasher.cs
+++ src/Medusa.Infrastructure/Identity/PasswordHasher.cs
@@ -1,33 +1,92 @@
+using System.Security.Cryptography;
+using System.Text;
+using Konscious.Security.Cryptography;
+
 namespace Medusa.Infrastructure.Identity;
 
 public class PasswordHasher : IPasswordHasher
 {
-    private const int WORK_FACTOR = 12;
+    private const int SALT_SIZE = 16;
+    private const int HASH_SIZE = 32;
+
+    private const int ARGON2_DEGREE_OF_PARALLELISM = 2;
+    private const int ARGON2_MEMORY_SIZE = 65536; // 64 MB
+    private const int ARGON2_ITERATIONS = 4;
 
     /// <inheritdoc />
-    public string ComputeHash(string password)
+    public async Task<string> ComputeHash(string password, CancellationToken cancellation = default)
     {
         if (string.IsNullOrWhiteSpace(password))
         {
             throw new ArgumentException("Password cannot be null or empty.", nameof(password));
         }
 
-        return BCrypt.Net.BCrypt.HashPassword(password, WORK_FACTOR);
+        var salt = RandomNumberGenerator.GetBytes(SALT_SIZE);
+
+        using var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
+        {
+            DegreeOfParallelism = ARGON2_DEGREE_OF_PARALLELISM,
+            MemorySize = ARGON2_MEMORY_SIZE,
+            Iterations = ARGON2_ITERATIONS,
+            Salt = salt,
+        };
+
+        var hash = await argon2.GetBytesAsync(HASH_SIZE);
+
+        var computed = new byte[SALT_SIZE + HASH_SIZE];
+
+        Buffer.BlockCopy(salt, 0, computed, 0, SALT_SIZE);
+        Buffer.BlockCopy(hash, 0, computed, SALT_SIZE, HASH_SIZE);
+
+        var saltedHash = Convert.ToBase64String(computed);
+
+        return saltedHash;
     }
 
     /// <inheritdoc />
-    public bool VerifyHash(string password, string hash)
+    public async Task<bool> VerifyHash(string password, string saltedHash, CancellationToken cancellation = default)
     {
         if (string.IsNullOrWhiteSpace(password))
         {
             throw new ArgumentException("Password cannot be null or empty.", nameof(password));
         }
 
-        if (string.IsNullOrWhiteSpace(hash))
+        if (string.IsNullOrWhiteSpace(saltedHash))
         {
-            throw new ArgumentException("Hash cannot be null or empty.", nameof(hash));
+            throw new ArgumentException("Salted hash cannot be null or empty.", nameof(saltedHash));
         }
 
-        return BCrypt.Net.BCrypt.Verify(password, hash);
+        var stored = Convert.FromBase64String(saltedHash);
+
+        if (stored.Length != SALT_SIZE + HASH_SIZE)
+        {
+            throw new FormatException("Invalid hash format.");
+        }
+
+        var salt = new byte[SALT_SIZE];
+        var hash = new byte[HASH_SIZE];
+
+        Buffer.BlockCopy(hash, 0, salt, 0, SALT_SIZE);
+        Buffer.BlockCopy(hash, SALT_SIZE, hash, 0, HASH_SIZE);
+
+        using var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
+        {
+            DegreeOfParallelism = ARGON2_DEGREE_OF_PARALLELISM,
+            MemorySize = ARGON2_MEMORY_SIZE,
+            Iterations = ARGON2_ITERATIONS,
+            Salt = salt,
+        };
+
+        var computed = await argon2.GetBytesAsync(HASH_SIZE);
+
+        for (int i = 0; i < HASH_SIZE; i++)
+        {
+            if (computed[i] != hash[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
